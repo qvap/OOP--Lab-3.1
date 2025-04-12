d4b91b6156ab0e0ae6310c45693c189695dc5413
qvap <135361555+qvap@users.noreply.github.com>
2025-04-12 22:16:45 +0500

    Initial commit

---
+++ lab3.1.py
@@ -0,0 +1,121 @@
+import customtkinter as ctk
+from tkinter import Canvas
+from math import sqrt
+
+class CCircle(): # Базовый класс круга
+
+    def __init__(self, canvas, x, y):
+        # Private
+        self.__radius = 50
+        self.__color = "#fc7f03"
+        self.__border_color = "#9c4d00"
+        self.__selected_border_color = "#FFFFFF"
+        self.__selected = False
+        self.__x = x
+        self.__y = y
+
+        # Public
+        self.canvas = canvas
+
+        print(f"New point: {x}, {y}")
+
+    def draw(self): # Отрисовывает круг на Canvas
+        __chosen_border_color = self.__selected_border_color if self.__selected else self.__border_color
+        self.canvas.create_oval(self.__x - self.__radius, self.__y - self.__radius,\
+            self.__x + self.__radius, self.__y + self.__radius, fill = self.__color, width = 5, outline = __chosen_border_color)
+    
+    def select(self): # "Выделяет" круг
+        self.__selected = True
+    
+    def deselect(self): # Снимает "выделение" с круга
+        self.__selected = False
+    
+    def mousecheck(self, x: int, y: int) -> bool: # Проверяет, наход. ли точка внутри круга
+        return ((x - self.__x)**2 + (y - self.__y)**2) <= self.__radius**2
+    
+    def destroy(self) -> bool: # Уничтожает себя
+        if self.__selected:
+            del self
+            return True
+        return False
+
+class Container():
+
+    def __init__(self, canvas):
+        # Private
+        self.__container = list()
+        self.__selected_container = list()
+        self.__multiple_selection = False
+
+        # Public
+        self.canvas = canvas
+    
+    def container_append(self, object: CCircle): # Добавляет в контейнер новый круг
+        print(f"Appending new object: {object}")
+        self.__container.append(object)
+    
+    def create_objects(self, point): # Создаёт новый круг и добавляет в список
+        self.container_append(CCircle(x=point.x, y=point.y, canvas=self.canvas))
+    
+    def select_objects(self, point): # Выделяет круги (в зависимости от __multiple_selection меняется поведение)
+        if not(self.__multiple_selection):
+            for circle in self.__selected_container:
+                circle.deselect()
+            self.__selected_container.clear()
+        
+        self.__selected_container.extend(list(filter(lambda x: x.mousecheck(point.x, point.y), self.__container)))
+        for circle in self.__selected_container:
+            circle.select()
+    
+    def deselect_objects(self, *args): # Снимает выделение со всех кругов
+        for circle in self.__selected_container:
+            circle.deselect()
+        self.__selected_container.clear()
+    
+    def delete_objects(self, *args): # Удаляет выделенные объекты
+        self.__container = [obj for obj in self.__container if not obj.destroy()]
+        self.__selected_container.clear()
+    
+    def initiate_selection(self, *args): # Включает множественное выделение
+        self.__multiple_selection = True
+
+    def stop_selection(self, *args): # Выключает множественное выделение
+        self.__multiple_selection = False
+    
+    def __getattribute__(self, name): # событие Paint
+        attr = super().__getattribute__(name)
+        if callable(attr):
+            def wrapper(*args, **kwargs):
+                result = attr(*args, **kwargs)
+                self.canvas.delete("all")
+                for circle in self.__container:
+                    circle.draw()
+                return result
+            return wrapper
+        return attr
+
+class App(ctk.CTk):
+
+    def __init__(self):
+        super().__init__()
+
+        self.title("Лабораторная работа №3")
+        self.geometry("400x600")
+        self.rowconfigure(0, weight=1)
+        self.columnconfigure(0, weight=1)
+
+        self.canvas = Canvas(master=self, bg="#24211e", highlightbackground="#24211e")
+        self.canvas.grid(row=0, column=0, sticky="nsew")
+
+        self.container = Container(canvas=self.canvas)
+
+        self.bind("<Button-1>", self.container.create_objects)
+        self.bind("<Button-3>", self.container.select_objects)
+        self.bind_all("<Escape>", self.container.deselect_objects)
+        self.bind_all("<Delete>", self.container.delete_objects)
+        self.bind("<KeyPress-Control_L>", self.container.initiate_selection)
+        self.bind("<KeyRelease-Control_L>", self.container.stop_selection)
+
+if __name__ == "__main__":
+    app = App()
+    app.mainloop()
